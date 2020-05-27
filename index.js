e5a088cf6d32e18eda9641b082921caa883a10bb
qudroturkey <reesemb1@gmail.com>
2020-05-26 19:07:26 -0500

    toggle almost works

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-$('button').click(() => {
+let open = false;
+
+function toggleEverything() {
   $('#main').toggle();
   $('#alternate').toggle();
+  open = !open;
+}
+
+$('i').click(() => {
+  if (open === false) {
+    $('#card-status').removeClass('card-opened');
+    $('#card-status').addClass('card-closed');
+    toggleEverything();
+  } else {
+    $('#card-status').removeClass('card-closed');
+    $('#card-status').addClass('card-opened');
+    toggleEverything();
+  }
 });
